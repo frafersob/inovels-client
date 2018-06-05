9cd1e2baba33cf24c5ade5c68fc481e923bb7af7
Francisco Fernández Sobejano <francisco.fernandezsobejano@alum.uca.es>
2018-06-05 17:54:53 +0200

    Added material support and initial novel card implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 import { AppComponent } from './app.component';
 import { NovelListComponent } from './novel-list.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    NovelListComponent
+    NovelListComponent, 
+    TimeAgoPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/novel-list.component.ts
+++ src/app/novel-list.component.ts
@@ -10,13 +10,13 @@ export class NovelListComponent {
     'name': 'Test Novel',
     'description': 'Description of test novel',
     'scenes': [],
-    'image': '/assets/image.jpg',
+    'image': '/assets/novelpage.jpg',
     'createDateTime': '2018-06-05T14:40:25',
     'updateDateTime': '2018-06-05T14:40:25'},
   {'id': 3, 'name': 'Test Novel 2',
     'description': 'Description of test novel 2',
     'scenes': [],
-    'image': '/assets/image.jpg',
+    'image': '/assets/novelpage.jpg',
     'createDateTime': '2018-06-05T14:40:25',
     'updateDateTime': '2018-06-05T14:40:25'}];
 }
